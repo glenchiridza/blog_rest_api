b669af529ad78bf310a272fa016f4dcf28cab800
glenchiridza <glenchiridz@gmail.com>
2021-09-19 21:16:26 +0200

    blogger post app

--- blogapi/urls.py
+++ blogapi/urls.py
@@ -6,7 +6,9 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rest-auth', include("rest_auth.urls")),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    path('api/token', obtain_auth_token, name="obtain-token")
+    # path('users/',include('users.urls'),name="users_update")
+    # path('api-auth',include('rest_framework.urls',namespace='rest_framework')),
+    # path('rest-auth', include("rest_auth.urls")),
+    # path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    # path('api/token', obtain_auth_token, name="obtain-token")
 ]
---
+++ blogger_post/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BloggerPostConfig(AppConfig):
+    name = 'blogger_post'
