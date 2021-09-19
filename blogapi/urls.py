099a3d03dab4c7987dde44f9dd2aff35b9a61632
glenchiridza <glenchiridz@gmail.com>
2021-09-19 20:05:48 +0200

    obtain auth token

--- blogapi/urls.py
+++ blogapi/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rest-auth',include("rest_auth.urls")),
     path('rest-auth/registration/',include('rest_auth.registration.urls')),
-    path
+    path('api/token',obtain_auth_token, name="obtain-token")
 ]
