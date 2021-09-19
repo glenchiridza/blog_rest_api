b4294fbdd8ad2629b3fb3fd93e3d28661e37ab91
glenchiridza <glenchiridz@gmail.com>
2021-09-19 20:36:50 +0200

    users

--- users/models.py
+++ users/models.py
@@ -10,7 +10,19 @@ class Writer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    profile_image = models.ImageField(upload_to="writ_profile/", max_length=255, blank=True, null=True)
     bio = models.TextField()
 
     def __str__(self):
-        return f"{self.user}"
+        return f"{self.user.username}"
+
+
+class Subscriber(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=100)
+    profile_image = models.ImageField(upload_to="sub_profile/", max_length=255, blank=True, null=True)
+    bio = models.TextField()
+
+    def __str__(self):
+        return f"{self.user.username}"
