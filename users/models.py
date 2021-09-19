6cf34c6bdc0538b92ea3ebea21759bb52d9b678b
glenchiridza <glenchiridz@gmail.com>
2021-09-19 20:27:07 +0200

    writer

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Writer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+    def __str__(self):
+        return f"{self.user}"
