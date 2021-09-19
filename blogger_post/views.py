9f27d584637b6b89419c42cf9f63db38743b6667
glenchiridza <glenchiridz@gmail.com>
2021-09-19 21:39:30 +0200

    post view

--- blogger_post/views.py
+++ blogger_post/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import BlogPost
+from .serializers import (BlogPostSerializer,
+                          UpdateBlogPostSerializer,
+                          DeleteBlogPostSerializer)
 
-# Create your views here.
+from rest_framework import mixins
+from rest_framework import generics
+
+
+class PostCreateView(mixins.ListModelMixin,
+                     mixins.CreateModelMixin,
+                     generics.GenericAPIView):
+    serializer_class = BlogPostSerializer
+    queryset = BlogPost.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(self, request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
