d302aac7aca90906df85b5ac61aab015aeb5c395
glenchiridza <glenchiridz@gmail.com>
2021-09-19 21:24:51 +0200

    Blogpost model

--- blogger_post/models.py
+++ blogger_post/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class BlogPost(models.Model):
+    author_name = models.OneToOneField(User, on_delete=models.CASCADE)
+    featured_image = models.ImageField(upload_to="featured/%Y/%m/%d/",blank=True,null=True)
+    title  = models.CharField(max_length=50)
+    subtitle = models.CharField(max_length=50,default='')
+    content = models.TextField()
+    post_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}"
